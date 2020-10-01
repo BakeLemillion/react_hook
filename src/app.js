3d9fc170fd1db56694527278a082f4856ddef4e2
KykiSemyen <iam-kyki@mail.ru>
2020-10-01 18:06:52 +0600

    01-10-2020

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-import React  from "react";
-import Routes from "./modules/routes"
-import TopBar from "./component/topBar"
-import {BrowserRouter as Router} from "react-router-dom"
+import React from "react";
+import Routes from "./modules/routes";
+import TopBar from "./component/topBar";
+import { BrowserRouter as Router } from "react-router-dom";
 
-function App () {
-    return(
-        <div className = 'app'>
-            <Router>
-                <TopBar />
-                <Routes />
-            </Router>
-            
-        </div>
-    )
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/modules/pages/authentication/index.js
+++ src/modules/pages/authentication/index.js
@@ -1,34 +1,62 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Authentication = () => {
-    return(
-        <div className="auth-page">
-            <div className="container page">
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 col-xs-12">
-                        <h1 className="text-xs-center"> 
-                            login
-                        </h1>
-                        <p className="text-xs-center">
-                            <Link to="/register">Need an account ?</Link>
-                        </p>
-                        <form>
-                            <fieldset>
-                                <fieldset className="form-group">
-                                    <input type="email" className="form-control form-control-lg" placeholder="Email" />
-                                </fieldset>
-                                <fieldset className="form-group">
-                                    <input type="password" className="form-control form-control-lg" placeholder="Password" />
-                                </fieldset>
-                                <button className="btn btn-lg btn-primary pull-xs-right" type="submit">Sign in</button>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Authentication
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("data", email, password);
+  };
+
+  useEffect(() => {
+    console.log("effect triggered");
+    document.title = email;
+  }, [email]);
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">login</h1>
+            <p className="text-xs-center">
+              <Link to="/register">Need an account ?</Link>
+            </p>
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </fieldset>
+                <button
+                  className="btn btn-lg btn-primary pull-xs-right"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
