bfdd7ed2974622499d2db754d8416ced738f843b
KykiSemyen <iam-kyki@mail.ru>
2020-10-20 13:54:46 +0600

    20-10-Feed and GlobalFeed page

--- src/modules/pages/globalFeed/index.js
+++ src/modules/pages/globalFeed/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Feed from "../../../component/feed";
+import useFetch from "../../../hooks/useFetch";
 
-const GlobalFeed = ()=> {
-    return (
-        <div>
-            GlobalFeed
+const GlobalFeed = () => {
+  const apiUrl = `/articles?limit=10&offset=0`;
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+  console.log("response", response);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium clone</h1>
+          <p>A place to share knowledge</p>
         </div>
-    )
-}
+      </div>
 
-export default GlobalFeed
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>isLoading...</div>}
+            {error && <div>Some error happend</div>}
+            {!isLoading && response && <Feed articles={response.articles} />}
+          </div>
+          <div className="col-md-3">Popular tags</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GlobalFeed;
