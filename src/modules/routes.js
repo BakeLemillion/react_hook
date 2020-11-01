b761164f58887e30e4d16f7b6998ee699aa58896
Baurjan Balgabaev <baurjan@pingocean.com>
2020-11-01 10:43:42 +0600

    tagPage is done

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import GlobalFeed from "./pages/globalFeed/index"
 import Article from "./pages/artical/index"
 import Authentication from "./pages/authentication"
+import TagFeed from './pages/tagFeed'
 
 export default () => {
     return (
         <Switch>
             <Route exact path="/" component={GlobalFeed} />
+            <Route exact path="/tags/:slug" component={TagFeed} />
             <Route exact path="/login" component={Authentication} />
             <Route exact path="/register" component={Authentication} />
             <Route path="/articles/:slug" component={Article} />
